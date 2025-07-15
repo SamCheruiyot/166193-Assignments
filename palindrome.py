1c8cff4a2aa01db67a864a4c9cff3b7364da033b
SamCheruiyot <1sammycheruiyot@gmail.com>
2025-07-15 11:05:49 +0300

    findingpalindromeusingstacks

---
+++ palindrome.py
@@ -0,0 +1,23 @@
+
+def is_palindrome(s):
+    stack=[]
+
+    for char in s:
+        stack.append(char)
+
+    reversed_s=""
+    while stack:
+        reversed_s += stack.pop()
+
+
+    if s == reversed_s:
+        print(f'"{s}" is a palindrome.')
+
+
+    else:
+        print(f'"{s}" is not a palindrome.')
+
+
+
+(is_palindrome("madam"))
+(is_palindrome("python"))
